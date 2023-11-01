92cd0fe47a8fd678146b9701231bf80d99c83004
Grantversioncon <149627499+Grantversioncon@users.noreply.github.com>
2023-11-01 16:26:14 -0500

    Update Base Servo code.ino

--- Base Servo code.ino
+++ Base Servo code.ino
@@ -15,7 +15,7 @@ void loop() {
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(12);
   digitalWrite(trigPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
